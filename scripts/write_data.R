afec503671fd4c8f46b9b20ec79fcf8cc9dd15e4
dshizuka <dshizuka2@unl.edu>
2023-08-27 21:00:27 -0500

    add scripts and data

---
+++ scripts/write_data.R
@@ -0,0 +1,4 @@
+### This script simply takes the iris dataset and saves it as a .csv file in the "data" subfolder
+
+write.csv(iris, "data/iris.csv")
+#note that I'm including "data/" in the file name. This places the file in that subfolder.
