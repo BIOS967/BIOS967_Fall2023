1540513a59a4ed61ffe06fb30ef2eba384176e06
dshizuka <dshizuka2@unl.edu>
2023-08-27 21:12:25 -0500

    update write_data

--- scripts/write_data.R
+++ scripts/write_data.R
@@ -2,3 +2,8 @@
 
 write.csv(iris, "data/iris.csv")
 #note that I'm including "data/" in the file name. This places the file in that subfolder.
+
+#Let's also generate a plot and save it to the "figures" subfolder. We'll get to an explanation of this code when we cover plotting, but try running this code.
+png("figures/plot.png")
+plot(Petal.Length~Species, data=iris)
+dev.off()
