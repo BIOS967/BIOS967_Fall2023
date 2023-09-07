822dabb2ba75e03e605fd233e0b8c54a9890d951
dshizuka <dshizuka2@unl.edu>
2023-09-07 13:49:01 -0500

    Create 20230907_dataerrors.R

---
+++ scripts/20230907_dataerrors.R
@@ -0,0 +1,51 @@
+#data import problems
+
+errdat=read.csv("data/SampleData_w_errors.csv")
+errdat
+
+#look at structure of data
+str(errdat)
+#problem: size is not a number here.
+errdat$size
+mean(errdat$size)
+
+#problem: weight is not a number
+errdat$weight
+#this problem is caused by the inclusion of #N/A! as a value.
+#solution: tell R that this is a missing value when importing the data.
+errdat=read.csv("data/SampleData_w_errors.csv", na.strings="#N/A!") #this tells R that "#N/A!" should be treated as NA
+errdat
+errdat$weight
+
+#to deal with the same problem in size, also designate "**" as NA
+errdat=read.csv("data/SampleData_w_errors.csv", na.strings=c("#N/A!","**"))
+errdat
+errdat$size
+mean(errdat$size, na.rm=T) #to calculate something from numeric vector with NAs, tell it to ignore NA using na.rm=T
+
+#to remove column "X":
+
+names(errdat) #give me the column names
+names(errdat)=="X" #are any of the columns called "X"?
+which(names(errdat)=="X") #which column is called "X"?
+
+errdat[,which(names(errdat)=="X")] #give me the values for the column called "X" in dataframe "errdat"
+
+dat=errdat[,which(names(errdat)!="X")] #give me all of the columns EXCEPT the one called "X"
+dat
+
+#let's remove any rows that is missing an individual ID
+dat$Indiv_ID
+dat2=dat[which(dat$Indiv_ID!=""),]
+dat2
+
+#plotting basics:
+
+plot(dat2$size, dat2$weight) #plot one continuous variable against another
+
+plot(dat2$weight~dat2$size) #same plot in 'formula' notation, i.e., y~x
+
+plot(weight~size, data=dat2) #alternative way: use variable names in the formula, then data= to designate the dataframe they come from.
+
+boxplot(weight~sex, data=dat2)
+
