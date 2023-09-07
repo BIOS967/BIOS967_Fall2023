979481da0833ecab2a17c3940f028efd3d39ffc8
AirFreshed <115960197+AirFreshed@users.noreply.github.com>
2023-09-07 13:48:01 -0500

    Updated and made more clear the assignment

--- Kugel_Ken/Homework1.R
+++ Kugel_Ken/Homework1.R
@@ -1,2 +1,10 @@
 #Homework 1
-#Group 2 Exercise
+
+# Group 2 Exercise --------------------------------------------------------
+
+
+
+
+
+# Finish Basic Plotting Module --------------------------------------------
+
