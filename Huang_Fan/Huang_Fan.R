2b6d34454ddf1167304b3f87cf5b3ed9d659a001
Fan <fhuang4@huskers.unl.edu>
2023-10-19 20:26:36 -0500

    2023-10-19

--- Huang_Fan/Huang_Fan.R
+++ Huang_Fan/Huang_Fan.R
@@ -545,5 +545,58 @@ filename %>%
   str_replace(".csv","") %>%
   ymd()
 
+who
+head(who)
+?who
+#isolating important files
+who_long=who %>%
+  pivot_longer(c(-country, -iso2, -iso3, -year))
+who_long %>% pull(name)%>% unique()
 
+#how to extract information from data
+#can bet sex as the 8th character
+who_long %>% pull(name)%>% str_sub(start=8, end = 8)
+
+#creating variable called sex
+who_long %>%
+  mutate(sex=str_sub(name, start = 8, end = 8))
+
+#get the method of diagnosis
+who_long %>% pull(name)%>% str_sub(start = 4, end = 6)%>%
+  str_replace("_","")
+#add new variables to who_long
+who_long %>%
+  mutate(sex=str_sub(name, start = 8, end = 8))%>%
+  mutate(method=str_sub(name, start=4, end = 6)%>%
+           str_replace("_", ""))
+
+#get age class
+who_long%>% pull(name) %>%
+  str_sub(start = 9) %>%
+  str_replace("014", "0-14") %>%
+  str_replace("1524", "15-24") %>%
+  str_replace("2534", "25-34") %>%
+  str_replace("3544", "35-44") %>%
+  str_replace("4554", "45-54") %>%
+  str_replace("5564", "55-64") %>%
+  str_replace("65", "65+") %>%
+  as.factor()
+
+who_use=who_long %>%
+  mutate(sex=str_sub(name, start = 8, end = 8))%>%
+  mutate(method=str_sub(name, start=4, end = 6)%>%
+           str_replace("_", ""))%>%
+  mutate(age=  str_sub(name, start = 9) %>%
+           str_replace("014", "0-14") %>%
+           str_replace("1524", "15-24") %>%
+           str_replace("2534", "25-34") %>%
+           str_replace("3544", "35-44") %>%
+           str_replace("4554", "45-54") %>%
+           str_replace("5564", "55-64") %>%
+           str_replace("65", "65+") %>%
+           as.factor())
+
+ggplot(who_use %>% filter(iso3=="USA") %>% filter(method=="sp") %>% filter(sex=="m"), aes(x=year, y=value, color=age))+
+  geom_point()+
+  geom_line()
 
