197e8d9cc2eddc3bd554f3628589e80731053d1b
JackRodenburg <rodenburgjack@gmail.com>
2023-12-14 18:39:19 -0600

    Did something
    Doing it

--- Rodenburg_Jack/9_26_23_data_wrangling.R
+++ Rodenburg_Jack/9_26_23_data_wrangling.R
@@ -24,31 +24,31 @@ billboard %>% filter(artist == "Backstreet Boys, The" | artist == "N'Sync") %>% 
 
 
 #SELECT BY COLUMNS
-billboard %>% select(artist, track, date.entered, wk1) 
-#UNselect columns by negative sign "-" 
+billboard %>% select(artist, track, date.entered, wk1)
+#UNselect columns by negative sign "-"
 billboard %>% select(-artist, -date.entered)
 
 ##shifting from "WIDE" format to "LONG" format using pivot_longer()
 billboard %>% pivot_longer(cols=starts_with("wk"), names_to = "week", values_to="rank")
 
 #cols= assigns the columns you want to post
-#values_to = asisigns the variable name for the values in those columns
-#names_to assigns the variablef name from what you'
+#values_to = assigns the variable name for the values in those columns
+#names_to assigns the variable name from what you'
 
-bb.summary= billboard %>% 
+bb.summary= billboard %>%
 
-  pivot_longer(cols=starts_with("wk"), names_to = "week", values_to="rank") %>% 
+  pivot_longer(cols=starts_with("wk"), names_to = "week", values_to="rank") %>%
   drop_na() %>%
-  group_by(artist) %>% 
+  group_by(artist) %>%
   summarise(artist.best=min(rank), n.weeks=n())
   #summarise(n.weeks=n())
-  
+
   #bb.summmary
   ggplot(bb.summary, aes(x=n.weeks, y=artist.best)) + geom_point()
 
 
 #create a new variable using mutate()
 #calculate rank change between week 1 and week 2 for each song
-billboard %>% 
-    mutate(rank.diff = wk2-wk1) %>% 
+billboard %>%
+    mutate(rank.diff = wk2-wk1) %>%
     select(artist, track, wk1, wk2, rank.diff)
